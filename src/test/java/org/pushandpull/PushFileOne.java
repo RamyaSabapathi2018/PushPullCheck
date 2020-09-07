4d32bfb0c4d1a5a501437c1fa8c7f1e2f23d7519
RamyaSabapathi2018 <mailramya26@gmail.com>
2020-09-07 17:11:52 +0530

    Adding into Master

---
+++ src/test/java/org/pushandpull/PushFileOne.java
@@ -0,0 +1,17 @@
+package org.pushandpull;
+
+public class PushFileOne {
+	public static void main(String[] args) {
+		int c=30;
+		int d= 45;
+		int temp=0;
+		temp = c;
+		c=d;
+		d=temp;
+		System.out.println("After Swap a value---->"+ c);
+		System.out.println("After Swap b value---->"+ d);
+				
+		
+	}
+
+}
