fd7a30fd77c37d9d5ac0cb0ddc0a1f5a8fe76e24
RamyaSabapathi2018 <mailramya26@gmail.com>
2020-09-07 17:28:23 +0530

    Added in sample1 branch

--- src/test/java/org/pushandpull/PushFileOne.java
+++ src/test/java/org/pushandpull/PushFileOne.java
@@ -10,7 +10,8 @@ public class PushFileOne {
 		d=temp;
 		System.out.println("After Swap a value---->"+ c);
 		System.out.println("After Swap b value---->"+ d);
-				
+		String s="1234";
+		System.out.println(s.length());		
 		
 	}
 
